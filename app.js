236a864df7fb923e9ec5038d5dd736071f329e60
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2023-04-25 17:04:22 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// export let x = 10;
+// export let y = 20;
+
+// in node js we can directly export or import the  variable we can do it via module.exports
+module.exports = {
+  x: 10,
+  y: 20,
+};
